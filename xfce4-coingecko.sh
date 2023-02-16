1831b0be66e4a6415e31d04bdccc1def2a634218
bimlas <bimbalaszlo@gmail.com>
2023-02-17 00:21:40 +0100

    Minimize the number of API queries
    Because the free plan of the API is rate-limited, I had to find a way to
    minimize the number of queries which is mostly done because of fetching
    token names (trending tokens list can change rapidly thus it has to
    fetch new token names).
    
    Fetch the whole token list only once, then filter for the needed tokens
    to get it's name.

--- xfce4-coingecko.sh
+++ xfce4-coingecko.sh
@@ -22,14 +22,26 @@ function alert_once()
   fi
 }
 
-function get_token_details()
+function get_token_list()
 {
-  token="${1}"; shift
   filename="${1}"; shift
 
   if [[ ! -f "${filename}" ]]; then
-    curl --silent "https://api.coingecko.com/api/v3/coins/${token}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false" > "${filename}"
+    curl --silent "https://api.coingecko.com/api/v3/coins/list?include_platform=false" > "${filename}"
   fi
+  fetch_error=$(_jq '.status.error_message' "${filename}")
+  if [[ "x${fetch_error}" != 'x' ]]; then
+    rm "${filename}"
+    return 1
+  fi
+}
+
+function get_token_prices()
+{
+  filename="${1}"; shift
+  tokens="${@}"
+
+  curl --silent "https://api.coingecko.com/api/v3/simple/price?ids=${tokens// /%2C}&vs_currencies=usd&include_24hr_change=true" > "${filename}"
   fetch_error=$(_jq '.status.error_message' "${filename}")
   if [[ "x${fetch_error}" != 'xnull' ]]; then
     rm "${filename}"
@@ -40,17 +52,10 @@ function get_token_details()
 function show_prices()
 {
   results=''
-  for token in "${@}"; do
-    token_filename="${price_filename}_${token}"
-
-    if [[ ! -f "${token_filename}" ]]; then 
-      get_token_details "${token}" "${token_filename}" || continue
-    fi
-
-    symbol=$(_jq '.symbol' "${token_filename}" | tr '[:lower:]' '[:upper:]')
-    id=$(_jq '.id' "${token_filename}")
-    price=$(_jq ".\"${token}\".usd" "${price_filename}")
-    change=$(_jq ".\"${token}\".usd_24h_change | . * 100.0 + 0.5 | floor / 100.0" "${price_filename}")
+  for token_id in "${@}"; do
+    symbol=$(_jq ".[] | select(.id == \"${token_id}\") | .symbol" "${tokens_filename}" | tr '[:lower:]' '[:upper:]')
+    price=$(_jq ".\"${token_id}\".usd" "${price_filename}")
+    change=$(_jq ".\"${token_id}\".usd_24h_change | . * 100.0 + 0.5 | floor / 100.0" "${price_filename}")
 
     price_color='green'
     if [[ $(echo "${change} < 0" | bc) -eq 1 ]]; then
@@ -58,7 +63,7 @@ function show_prices()
     fi
 
     change_color=${price_color}
-    state_filename="${token_filename}_alert"
+    state_filename="${tokens_filename}_${token_id}_alert"
     if [[ $(echo "${change#-} > ${warninglevel}" | bc) -eq 1 ]]; then
       alert_once "Huge price change on ${symbol}" "${state_filename}"
       change_color='yellow'
@@ -68,7 +73,7 @@ function show_prices()
     fi 
 
     if [[ -n "${TOOLTIP:+is_set}" ]]; then
-      results="$results\n${symbol} ${id} \$${price} (${change}%)"
+      results="$results\n${symbol} ${token_id} \$${price} (${change}%)"
     else
       results="$results ${symbol}: <span foreground=\"${price_color}\">\$${price}</span> <span foreground=\"${change_color}\">(${change}%)</span>"
     fi
@@ -80,15 +85,11 @@ warninglevel="${1}"; shift
 tokens="${@}"
 trending_tokens=$(curl --silent 'https://api.coingecko.com/api/v3/search/trending' | _jq '.coins [].item .id' | tr '\n' ' ')
 price_filename='/tmp/xfce-genmon-coingecko'
+tokens_filename="${price_filename}_tokens"
 
-curl --silent "https://api.coingecko.com/api/v3/simple/price?ids=${trending_tokens// /%2C}${tokens// /%2C}&vs_currencies=usd&include_24hr_change=true" > "${price_filename}"
+(get_token_list "${tokens_filename}" && get_token_prices "${price_filename}" ${tokens} ${trending_tokens}) || (echo "<txt>Error while fetching CoinGecko</txt>" && exit 1)
 
-fetch_error=$(_jq '.status.error_message' "${price_filename}")
-if [[ "x${fetch_error}" != 'xnull' ]]; then
-  echo "<txt>Error while fetching CoinGecko</txt><tool>${fetch_error}</tool>"
-else
-  echo -e \
-    "<txt>$(show_prices ${tokens})</txt>" \
-    "<txtclick>xdg-open 'https://www.coingecko.com/'</txtclick>" \
-    "<tool>Click to visit CoinGecko\n\nTrending tokens:\n\n<span font-family=\"monospace\" allow-breaks=\"false\">$(TOOLTIP=1 show_prices ${trending_tokens} | column -t)</span>\n\n<small>(last update: $(date))</small></tool>"
-fi
+echo -e \
+  "<txt>$(show_prices ${tokens})</txt>" \
+  "<txtclick>xdg-open 'https://www.coingecko.com/'</txtclick>" \
+  "<tool>Click to visit CoinGecko\n\nTrending tokens:\n\n<span font-family=\"monospace\" allow-breaks=\"false\">$(TOOLTIP=1 show_prices ${trending_tokens} | column -t)</span>\n\n<small>(last update: $(date))</small></tool>"
